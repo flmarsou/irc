09a4f83de81ebec157ab77e56fd8742da2e2062b
flmarsou <marsouinflorent12@gmail.com>
2025-05-26 16:17:56 +0200

    Added parser for arguments (port)

---
+++ src/parser.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/26 10:01:06 by flmarsou          #+#    #+#             */
+/*   Updated: 2025/05/26 12:49:29 by flmarsou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "irc.hpp"
+
+static bool	checkArgc(const int argc, const char **argv)
+{
+	if (argc != 3)
+	{
+		std::cerr << INFO "./ircserv <port> <password>" << std::endl;
+		return (false);
+	}
+	if (!argv[1][0])
+	{
+		std::cerr << ERROR "Missing Port!" << std::endl;
+		return (false);
+	}
+	if (!argv[2][0])
+	{
+		std::cerr << ERROR "Missing Password!" << std::endl;
+		return (false);
+	}
+	return (true);
+}
+
+static bool	checkPort(const char *argv1, unsigned short &port)
+{
+	char		*end;
+	const int	temp = std::strtol(argv1, &end, 10);
+
+	if (*end != '\0')
+	{
+		std::cerr << ERROR "Invalid port, must be a number!" << std::endl;
+		std::cerr << INFO "Expected a numeric value in the range [1 - 65535]" << std::endl;
+		return (false);
+	}
+	if (temp < 1)
+	{
+		std::cerr << ERROR "Port too small, must be at least 1!" << std::endl;
+		std::cerr << INFO "Valid port range is [1 - 65535]" << std::endl;
+		return (false);
+	}
+	if (temp > 65535)
+	{
+		std::cerr << ERROR "Port too large, maximum allowed is 65535!" << std::endl;
+		std::cerr << INFO "Valid port range is [1 - 65535]" << std::endl;
+		return (false);
+	}
+	port = temp;
+	return (true);
+}
+
+bool	parser(const int argc, const char **argv, unsigned short &port)
+{
+	if (!checkArgc(argc, argv))
+		return (false);
+	if (!checkPort(argv[1], port))
+		return (false);
+
+	return (true);
+}
