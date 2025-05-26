64e64d6b473f843375f68ddb2bc5c0533eaf602c
flmarsou <marsouinflorent12@gmail.com>
2025-05-26 16:17:34 +0200

    Create debug.hpp

---
+++ includes/debug.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/26 10:47:19 by flmarsou          #+#    #+#             */
+/*   Updated: 2025/05/26 13:33:00 by flmarsou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <iostream>
+
+# define ERROR		"\e[31m[x] - Error: \e[0m"
+# define SUCCESS	"\e[32m[v] - Success: \e[0m"
+# define WARNING	"\e[35m[!] - Warning: \e[0m"
+# define INFO		"\e[36m[i] - Info: \e[0m"
