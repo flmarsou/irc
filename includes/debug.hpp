8ae0d77455b7ce193e2602f72a71b0b978323327
flmarsou <marsouinflorent12@gmail.com>
2025-05-27 16:58:44 +0200

    Added message flag

--- includes/debug.hpp
+++ includes/debug.hpp
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 10:47:19 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/05/26 13:33:00 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/05/27 16:34:53 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,4 @@
 # define SUCCESS	"\e[32m[v] - Success: \e[0m"
 # define WARNING	"\e[35m[!] - Warning: \e[0m"
 # define INFO		"\e[36m[i] - Info: \e[0m"
+# define MSG		"\e[33m[#] - Message: \e[0m"
