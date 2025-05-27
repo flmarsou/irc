5db3f3084abc5557e3cd780bb7f11264e01652d8
flmarsou <marsouinflorent12@gmail.com>
2025-05-27 16:59:37 +0200

    Changed char to string for password

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,10 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 09:44:30 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/05/26 15:46:10 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/05/27 16:40:10 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "irc.hpp"
 #include "Server.hpp"
 
 int	main(const int argc, const char **argv)
@@ -22,12 +21,12 @@ int	main(const int argc, const char **argv)
 
 	try
 	{
-		Server serv(port);
+		Server serv(port, std::string(argv[2]));
 		serv.run();
 	}
-	catch(const std::exception &exception)
+	catch (const std::exception &e)
 	{
-		std::cerr << exception.what() << std::endl;
+		std::cerr << e.what() << std::endl;
 	}
 
 	return (0);
