df617665f89e2900393f3a259cee3d12c7361196
flmarsou <marsouinflorent12@gmail.com>
2025-06-03 15:31:37 +0200

    Added username/realname setters & getters

--- src/Client.cpp
+++ src/Client.cpp
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 10:57:01 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/06/03 14:07:48 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/06/03 14:38:00 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 // ========================================================================== //
 
 Client::Client(const int fd, const char *ip, const unsigned short port)
-	:	_fd(fd), _ip(ip), _port(port), _nick(""), _user(""), _hasPassword(false)
+	:	_fd(fd), _ip(ip), _port(port), _hasPassword(false), _nickname(""), _username(""), _realname("")
 {
 	std::cout << SUCCESS "!" << std::endl;
 	std::cout << INFO "New client (fd=" << this->_fd << ") " << this->_ip << ":" << this->_port << " connected!" << std::endl;
@@ -25,25 +25,28 @@ Client::Client(const int fd, const char *ip, const unsigned short port)
 
 Client::~Client()
 {
-	if (this->_nick.empty())
+	if (this->_nickname.empty())
 		std::cout << INFO "Client (fd=" << this->_fd << ") deleted!" << std::endl;
 	else
-		std::cout << INFO "Client " << this->_nick << " (fd=" << this->_fd << ") deleted!" << std::endl;
+		std::cout << INFO "Client " << this->_nickname << " (fd=" << this->_fd << ") deleted!" << std::endl;
 }
 
 // ========================================================================== //
 //   Setters & Getters                                                        //
 // ========================================================================== //
 
-int					Client::getFD() const						{ return (this->_fd); }
-const char			*Client::getIP() const						{ return (this->_ip); }
-unsigned short		Client::getPort() const						{ return (this->_port); }
+int					Client::getFD() const								{ return (this->_fd); }
+const char			*Client::getIP() const								{ return (this->_ip); }
+unsigned short		Client::getPort() const								{ return (this->_port); }
 
-void				Client::setPassword(const bool hasPassword)	{ this->_hasPassword = hasPassword; }
-bool				Client::getPassword() const					{ return (this->_hasPassword); }
+void				Client::setPassword(const bool hasPassword)			{ this->_hasPassword = hasPassword; }
+bool				Client::getPassword() const							{ return (this->_hasPassword); }
 
-void				Client::setNick(const std::string &nick)	{ this->_nick = nick; }
-const std::string	&Client::getNick() const					{ return (this->_nick); }
+void				Client::setNickname(const std::string &nickname)	{ this->_nickname = nickname; }
+const std::string	&Client::getNickname() const						{ return (this->_nickname); }
 
-void				Client::setUser(const std::string &user)	{ this->_user = user; }
-const std::string	&Client::getUser() const					{ return (this->_user); }
+void				Client::setUsername(const std::string &username)	{ this->_username = username; }
+const std::string	&Client::getUsername() const						{ return (this->_username); }
+
+void				Client::setRealname(const std::string &realname)	{ this->_realname = realname; }
+const std::string	&Client::getRealname() const						{ return (this->_realname); }
--- src/Client.hpp
+++ src/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 10:52:03 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/06/03 13:36:22 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/06/03 14:37:00 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,22 @@ class	Client
 		void				setPassword(const bool hasPassword);
 		bool				getPassword() const;
 
-		void				setNick(const std::string &nick);
-		const std::string	&getNick() const;
+		void				setNickname(const std::string &nickname);
+		const std::string	&getNickname() const;
 
-		void				setUser(const std::string &user);
-		const std::string	&getUser() const;
+		void				setUsername(const std::string &username);
+		const std::string	&getUsername() const;
+
+		void				setRealname(const std::string &realname);
+		const std::string	&getRealname() const;
 
 	private:
 		const int		_fd;
 		const char		*_ip;
 		unsigned short	_port;
 		bool			_hasPassword;
-		std::string		_nick;
-		std::string		_user;
+		std::string		_nickname;
+		std::string		_username;
+		std::string		_realname;
+
 };
