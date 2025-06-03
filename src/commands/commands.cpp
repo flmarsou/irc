98232e3976209b01bbf52d6ac70096f4c5350d9e
flmarsou <marsouinflorent12@gmail.com>
2025-06-03 13:31:52 +0200

    Better command handler

---
+++ src/commands/commands.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   commands.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/28 14:04:57 by flmarsou          #+#    #+#             */
+/*   Updated: 2025/06/03 13:30:19 by flmarsou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Server.hpp"
+
+// ========================================================================== //
+//   Utils                                                                    //
+// ========================================================================== //
+
+static const std::string	trimInput(std::string input)
+{
+	// Removes all the starting spaces
+	for (unsigned int i = 0; input[i] == ' ';)
+		input.erase(i, 1);
+
+	// Removes all the whitespaces
+	for (unsigned int i = 0; input[i];)
+	{
+		if (input[i] == '\n' || input[i] == '\r')
+			input.erase(i, 1);
+		else
+			i++;
+	}
+
+	return (input);
+}
+
+// ========================================================================== //
+//   Method                                                                   //
+// ========================================================================== //
+
+void	Server::commands(int index, std::string &input)
+{
+	const std::map<int, Client>::iterator	it = this->_clients.find(this->_fds[index].fd);
+
+	if (!it->second.getPassword())
+	{
+		if (input.substr(0, 5) == "PASS ")
+			commandPass(it, trimInput(input.substr(5)));
+		else
+		{
+			const std::string	numerical = ERR_NOTREGISTERED;
+			send(it->first, numerical.c_str(), numerical.size(), 0);
+		}
+		return ;
+	}
+
+	if (input.substr(0, 5) == "NICK ")
+		commandNick(it, trimInput(input.substr(5)));
+	else if (input.substr(0, 5) == "PASS ")
+		commandPass(it, trimInput(input.substr(5)));
+}
