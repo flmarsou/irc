5e7df6cfaa9a275ee8d25525170017f7873db525
flmarsou <marsouinflorent12@gmail.com>
2025-06-03 13:31:22 +0200

    Added PASS errors

--- includes/errors.hpp
+++ includes/errors.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error.hpp                                          :+:      :+:    :+:   */
+/*   errors.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 15:37:41 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/06/02 15:59:34 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/06/03 13:31:08 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,16 @@
 
 // Returned when a NICK message is processed that results in an attempt to change to a currently existing nickname.
 # define ERR_NICKNAMEINUSE(nick)	"433 " + nick + " :Nickname is already in use\n"
+
+// ========================================================================== //
+//   PASS                                                                     //
+// ========================================================================== //
+
+// Returned by the server to indicate that the client must be registered before the server will allow it to be parsed in detail.
+# define ERR_NOTREGISTERED		"451 :You have not registered\n"
+
+// Returned by the server to any link which tries to change part of the registered details.
+# define ERR_ALREADYREGISTRED	"462 :You may not reregister\n"
+
+// Returned to indicate a failed attempt at registering a connection for which a password was required and was either not given or incorrect.
+# define ERR_PASSWDMISMATCH		"464 :Password incorrect\n"
