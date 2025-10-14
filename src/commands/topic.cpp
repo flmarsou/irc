2d0d3e5d3fb88e4562176b06693bc3d65a974eb5
flmarsou <marsouinflorent12@gmail.com>
2025-10-14 10:59:59 +0200

    Fixed TOPIC

--- src/commands/topic.cpp
+++ src/commands/topic.cpp
@@ -6,13 +6,16 @@
  */
 void	Server::topic(Client *client, const std::vector<std::string> &tokens, u32 tokenSize)
 {
+	// Missing channel name
 	if (tokenSize < 2)
 	{
 		client->SendMessage(ERR_NEEDMOREPARAMS(tokens[0]));
 		return ;
 	}
 
-	Channel	*channel;
+	// Look for the channel
+	Channel	*channel = NULL;
+
 	for (u32 i = 0; i < _channels.size(); ++i)
 	{
 		if (tokens[1] == _channels[i]->GetName())
@@ -20,13 +23,16 @@ void	Server::topic(Client *client, const std::vector<std::string> &tokens, u32 t
 			channel = _channels[i];
 			break ;
 		}
-		if (i == _channels.size())
-		{
-			client->SendMessage(ERR_NOSUCHCHANNEL(tokens[1]));
-			return ;
-		}
 	}
 
+	// No such channel found
+	if (!channel)
+	{
+		client->SendMessage(ERR_NOSUCHCHANNEL(tokens[1]));
+		return ;
+	}
+
+	// Send Topic
 	if (tokenSize == 2)
 	{
 		if (channel->GetTopic().empty())
@@ -36,12 +42,14 @@ void	Server::topic(Client *client, const std::vector<std::string> &tokens, u32 t
 		return ;
 	}
 
+	// Set Topic & Permission Check
 	if (channel->GetTopicPerm() && !channel->IsOperator(client->GetNickname()))
 	{
 		client->SendMessage(ERR_CHANOPRIVSNEEDED(tokens[1]));
 		return ;
 	}
 
+	// Concatenate new topic from remaining tokens
 	std::string	topicMessage;
 
 	for (u32 i = 2; i < tokens.size(); ++i)
@@ -51,8 +59,12 @@ void	Server::topic(Client *client, const std::vector<std::string> &tokens, u32 t
 			topicMessage += ' ';
 	}
 
+	// Update Topic & Notify Topic & Debug
 	channel->SetTopic(topicMessage);
-	channel->Broadcast(RAW_TOPIC(client->GetNickname(), client->GetUsername(), client->GetIP(), channel->GetName(), channel->GetTopic()), client->GetNickname());
-	client->SendMessage(RAW_TOPIC(client->GetNickname(), client->GetUsername(), client->GetIP(), channel->GetName(), channel->GetTopic()));
+
+	const std::string	&rawTopic = RAW_TOPIC(client->GetNickname(), client->GetUsername(), client->GetIP(), channel->GetName(), channel->GetTopic());
+	channel->Broadcast(rawTopic, client->GetNickname());
+	client->SendMessage(rawTopic);
+
 	std::cout << MSG << client->GetNickname() << " changed " << channel->GetName() << "'s topic to \"" << channel->GetTopic() << "\"" << RESET << std::endl;
 }
